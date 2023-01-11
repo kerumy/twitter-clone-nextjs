b828104f201edbad7622c8567b3b98368092ffba
kerumy <harubo97521@gmail.com>
2023-01-11 22:30:22 +0900

    add sanitycli

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from 'sanity/lib/exports/cli'
+import {defineCliConfig} from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
